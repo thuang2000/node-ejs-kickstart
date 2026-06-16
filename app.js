1fabaa2442507ca4a56d6e1a9da4463c8267e7dd
thuang2000 <thuangnsc@gmail.com>
2026-06-16 03:34:01 +0000

    Added pages and images

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 
 //index/home URL
 app.get('/',(req,res)=>{
-    let title = "Home Page";
+    let title = "World Cup Favorites Home Page";
     res.render('pages/index',{'title': title});
 });
 
@@ -24,6 +24,31 @@ app.get('/about',(req,res)=>{
     let title = "About Page";
     res.render('pages/about',{'title': title});
 });
+
+//argentina URL
+app.get('/argentina',(req,res)=>{
+    let title = "About Argentina";
+    res.render('pages/argentina',{'title': title});
+});
+
+//portugal URL
+app.get('/portugal',(req,res)=>{
+    let title = "About Portugal";
+    res.render('pages/portugal',{'title': title});
+});
+
+//spain URL
+app.get('/spain',(req,res)=>{
+    let title = "About Spain";
+    res.render('pages/spain',{'title': title});
+});
+
+//france URL
+app.get('/france',(req,res)=>{
+    let title = "About France";
+    res.render('pages/france',{'title': title});
+});
+
 
 //Set server to listen for requests
 app.listen(port, () => {
